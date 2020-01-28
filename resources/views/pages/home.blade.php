197eb05216caf8a51071525c9656dd240cc8ae02
Stefan Drobnjak <drobnjak.stefan18@gmail.com>
2020-01-28 09:39:57 +0100

    Home page izmene

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -546,25 +546,25 @@
                         </div>
                     </div>
                 </div>
-                <nav aria-label="pagination-wrapper" class="pagination-wrapper">
-                    <ul class="pagination justify-content-center">
-                        <li class="page-item active">
-                            <a class="page-link" href="#" aria-label="Previous">
-                                <span aria-hidden="true"><i class="fa fa-angle-double-left mr-2"></i></span>
-                                <span class="">Previous</span>
-                            </a>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Next">
-                                <span class="">Next</span>
-                                <span aria-hidden="true"><i class="fa fa-angle-double-right ml-2"></i></span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+{{--                <nav aria-label="pagination-wrapper" class="pagination-wrapper">--}}
+{{--                    <ul class="pagination justify-content-center">--}}
+{{--                        <li class="page-item active">--}}
+{{--                            <a class="page-link" href="#" aria-label="Previous">--}}
+{{--                                <span aria-hidden="true"><i class="fa fa-angle-double-left mr-2"></i></span>--}}
+{{--                                <span class="">Previous</span>--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
+{{--                        <li class="page-item"><a class="page-link" href="#">1</a></li>--}}
+{{--                        <li class="page-item"><a class="page-link" href="#">2</a></li>--}}
+{{--                        <li class="page-item"><a class="page-link" href="#">3</a></li>--}}
+{{--                        <li class="page-item">--}}
+{{--                            <a class="page-link" href="#" aria-label="Next">--}}
+{{--                                <span class="">Next</span>--}}
+{{--                                <span aria-hidden="true"><i class="fa fa-angle-double-right ml-2"></i></span>--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
+{{--                    </ul>--}}
+{{--                </nav>--}}
             </div>
             <div class="col-lg-4 col-md-8 col-sm-12 col-xs-12">
                 <div class="sidebar sidebar-right">
