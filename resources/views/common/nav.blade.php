45dfa172810b1510d67282a6fca97d63fba513f0
Stefan <drobnjak.stefan18@gmail.com>
2020-11-17 23:03:10 +0100

    Home return

--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -104,7 +104,7 @@
                         <ul class="navbar-nav mr-auto">
 
                             <li class="nav-item dropdown">
-                                <a class="nav-link" href="{{route('home')}}">Pocetna</a>
+                                <a class="nav-link" href="{{route('home')}}">Home</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link" href="{{route('category','Football')}}">Football</a>
