49fe214f1ff0bd53e2eaf7b2a9cbf73b919a0a7b
Stefan <drobnjak.stefan18@gmail.com>
2020-12-02 18:32:18 +0100

    update views seeder

---
+++ database/seeds/UpdateViewsPostTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UpdateViewsPostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $ids = [];
+
+        for ($i = 1; $i <= 20; $i++){
+            array_push($ids, $i);
+        }
+        DB::table('posts')->whereIn('id', $ids)->update(['views' => 1]);
+    }
+}
